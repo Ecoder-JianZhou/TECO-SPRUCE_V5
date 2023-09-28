23ad761df18ef042a00814cdb7a52b71edb259c1
Ecoder-JianZhou <ecoder-zj@outlook.com>
2023-09-28 18:17:17 -0400

    update

---
+++ make_teco_desktop.sh
@@ -0,0 +1,32 @@
+
+gfortran -g -fbacktrace -Wall -fcheck=all   \
+    src/teco/datatypes.f90                  \
+    src/teco/updateAndSummary.f90          \
+    src/teco/io_mod.f90            \
+    src/teco/soil.f90                      \
+    src/teco/vegetation.f90                 \
+    src/teco/transfer.f90                  \
+    `# src/tools/mcmc_functions.f90`            \
+    `# src/tools/mcmc_outputs.f90`              \
+    src/teco/driver.f90                    \
+    `# src/tools/mcmc.f90 `                     \
+    `# src/tools/spinup.f90`                    \
+    src/main.f90                            \
+    -o run_teco                             \
+    -I"C:\Users\jz964\AppData\Local\miniconda3\envs\spruce-da\Library\include" -L"C:\Users\jz964\AppData\Local\miniconda3\envs\spruce-da\Library\lib" -lnetcdff -lnetcdf
+
+if find "src" -name "*.mod" -print -quit | grep -q '.*'; then
+    rm src/*.mod
+fi
+
+if find "src/teco" -name "*.mod" -print -quit | grep -q '.*'; then
+    rm src/teco/*.mod
+fi
+
+if find "src/tools" -name "*.mod" -print -quit | grep -q '.*'; then
+    rm src/tools/*.mod
+fi
+
+if find "." -name "*.mod" -print -quit | grep -q '.*'; then
+    rm *.mod
+fi
